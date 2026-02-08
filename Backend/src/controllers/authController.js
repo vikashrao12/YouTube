7566b466b235577174a9cb8b748bc85774be4db9
vikashrao12 <vikashrao12@gmail.com>
2026-02-08 21:33:30 +0530

    Impliment login functinality

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 
 export const registerUser = async (req, res) => {
   try {
@@ -28,5 +30,61 @@ export const registerUser = async (req, res) => {
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     res.status(500).json({ message: "Register error" });
+  }
+};
+
+
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+
+    
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "email and password required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+       
+    if (!user) {
+      return res.status(400).json({
+        message: "user not found",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+   
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "wrong password",
+      });
+    }
+
+    // token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_KEY,
+      { expiresIn: "7d" }
+    );
+ 
+
+    res.json({
+      message: "login success",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    console.log("login error")
+    res.status(500).json({
+      message: "server error",
+    });
   }
 };
--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { registerUser } from "../controllers/authController.js";
+import { loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
