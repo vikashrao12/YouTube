10712ec379cd4c2324f49c54eee468533b8c2da6
vikashrao12 <vikashrao12@gmail.com>
2026-02-08 12:29:57 +0530

    Setup React Router and create base application pages

---
+++ src/pages/Channel.jsx
@@ -0,0 +1,9 @@
+function Channel() {
+  return (
+    <div>
+      <h1>Channel Page</h1>
+    </div>
+  );
+}
+
+export default Channel;
---
+++ src/pages/VideoPlayer.jsx
@@ -0,0 +1,9 @@
+function VideoPlayer() {
+  return (
+    <div>
+      <h1>Video Player Page</h1>
+    </div>
+  );
+}
+
+export default VideoPlayer;
